f98267f8983e5e5e3a1da875ed9551d95890eca3
wangbingzhong7-creator <wangbingzhong7@gmail.com>
2026-05-13 19:39:55 +0800

    Add files via upload

--- app.py
+++ app.py
@@ -274,7 +274,8 @@ def qrcode_image():
     return send_file(buf, mimetype='image/png', as_attachment=True, download_name='点菜二维码.png')
 
 
+init_db()
+
 if __name__ == '__main__':
-    init_db()
     port = int(os.environ.get('PORT', 5000))
     app.run(host='0.0.0.0', port=port, debug=not IS_PG)
